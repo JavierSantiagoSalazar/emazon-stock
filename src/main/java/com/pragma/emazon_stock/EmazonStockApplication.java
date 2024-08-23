d42a9a9f7425e734db3769b17647ceb3d004f37e
Javier Santiago <javiersantiago45011@gmail.com>
2024-08-23 12:10:53 -0500

    base commit

---
+++ src/main/java/com/pragma/emazon_stock/EmazonStockApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonStockApplication.class, args);
+	}
+
+}
